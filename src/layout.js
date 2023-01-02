da1356987f2b87353226f84d77b7162562a9c474
faldea1 <faldea1@uc.cl>
2023-01-02 19:26:57 +0000

    Start Star Wars Project

---
+++ src/layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+const Layout = () => {
+    return (
+          <div>Star Wars</div>  
+    );
+
+};
+
+export default Layout;
